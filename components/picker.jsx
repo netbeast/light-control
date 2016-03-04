b2489c45c0659daa952af91a9d966f2cc8469180
pablopi <pablopizarroaguilar@gmail.com>
2016-03-04 16:40:24 +0100

    First Commit

---
+++ components/picker.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import ColorPicker from 'react-color'
+import netbeast from 'netbeast'
+
+export default class Picker extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      color: '#fff',
+      power: false
+    }
+    this.handleChangeComplete = this.handleChangeComplete.bind(this)
+  }
+
+  handleChangeComplete (color) {
+    console.log(color)
+    this.setState({ color: color.hex })
+    this.sendData({ power: this.state.power, color: color.hex })
+  }
+
+  handleClick (power) {
+    this.setState({ power: power })
+    this.sendData({ power: power, color: this.state.color })
+  }
+
+  sendData (status) {
+    netbeast('lights').set(status)
+  }
+
+  render () {
+    return (
+      <div>
+        <ColorPicker color={ this.state.color } type='material' onChangeComplete={ this.handleChangeComplete } />
+        <button onClick={ this.handleClick.bind(this, true) }>On</button>
+        <button onClick={ this.handleClick.bind(this, false) }>Off</button>
+        <ColorPicker color={ this.state.color } type='slider' onChangeComplete={ this.handleChangeComplete } />
+      </div>
+    )
+  }
+}
